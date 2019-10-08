1888a9c3d2bc308d80b1e6f2eb7598b15a316575
Serene <unexpecte-d@hotmail.com>
2019-10-08 14:37:18 +0800

    [FEAT] Add new google fonts and changed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,14 +25,14 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/SR_Circle.png`, // This path is relative to the root of the site.
       },
     },
     {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ["Bitter", "Droid Serif"],
+          families: ["Karla", "Bitter"],
         },
       },
     },
